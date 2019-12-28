18098152146cca6312c555aa1ebc8208bcb0b3d1
tonnac <tonnac35@gmail.com>
2019-12-28 12:57:51 +0900

    Momentum - 2.7 DOM - If else - Function practice part Two

--- Projects/momentum/index.js
+++ Projects/momentum/index.js
@@ -1,1 +1,13 @@
-console.log("Im Working. Im JS.");
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+  title.classList.toggle(CLICKED_CLASS);
+}
+
+function init() {
+  title.addEventListener("click", handleClick);
+}
+
+init();
